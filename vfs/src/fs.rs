a1d2d9c342b15d1d43c36f931f25d31bf03149b6
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2024-01-27 16:09:05 +0100

    add convience methods to fs

--- vfs/src/fs.rs
+++ vfs/src/fs.rs
@@ -1,4 +1,12 @@
-use crate::{error::Error, vfs_box, VFSBox, VPath};
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
+
+use crate::{
+    error::{Error, ErrorKind},
+    vfs_box, OpenOptions, VFSBox, VFileExt, VPath,
+};
 
 pub trait VFS: Send + Sync + Sized {
     type Path: VPath<FS = Self>;
@@ -14,6 +22,22 @@ pub trait VFSExt: VFS {
         Self::Path: Send + Sync + Clone,
     {
         vfs_box(self)
+    }
+
+    fn read(&self, path: &str) -> Result<Vec<u8>, Error> {
+        let path = self.path(path)?;
+
+        let mut file = path.open(OpenOptions::default().read(true))?;
+
+        let mut buffer = Vec::default();
+        file.read_to_end(&mut buffer)?;
+
+        Ok(buffer)
+    }
+
+    fn read_to_string(&self, path: &str) -> Result<String, Error> {
+        let buffer = self.read(path)?;
+        String::from_utf8(buffer).map_err(|err| Error::new(ErrorKind::InvalidData, err.to_string()))
     }
 }
 
